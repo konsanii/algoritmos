ee66dc696abc3c561a4d84f9557bcea9402a3c17
lucas consani <lucas_consani1@hotmail.com>
2024-03-18 22:38:23 -0300

    exercicio da aula 05

---
+++ aula05/exemplo 01.py
@@ -0,0 +1,5 @@
+idade = input("qual sua idade")
+if idade >= 18:
+    print("vc ja é de maior")
+else:
+    print("vc ainda não e de maior")
---
+++ aula05/exercicio 1.py
@@ -0,0 +1,6 @@
+x1 = (input("digite o primeiro numero"))
+x2 = (input("digite o segundo numero"))
+if x1 > x2:
+       print("maior", x1)
+else:
+       print("menor", x1)
